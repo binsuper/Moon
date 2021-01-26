4cc89eeca487b41f5c8b4e6da0a791348874c8cf
ginohuang <ginohuang@acingame.com>
2021-01-26 18:21:44 +0800

    则æ增加重连的错误处理

--- src/Moon.php
+++ src/Moon.php
@@ -422,6 +422,21 @@ class Moon {
 
     const ERROR_SERVER_INTERRUPT = " MySQL server has gone away";
 
+    // mysql 连接断开异常
+    const ERROR_MYSQL_BREAK = [
+        'server has gone away',
+        'no connection to the server',
+        'Lost connection',
+        'is dead or not enabled',
+        'Error while sending',
+        'decryption failed or bad record mac',
+        'server closed the connection unexpectedly',
+        'SSL connection has been closed unexpectedly',
+        'Error writing data to the connection',
+        'Resource deadlock avoided',
+        'failed with errno',
+    ];
+
     protected static $global_options;
     private static   $_instance;
     private static   $hash_cnt      = 0;
@@ -734,8 +749,10 @@ class Moon {
             try {
                 return call_user_func($action);
             } catch (\PDOException $ex) {
-                if (false !== strstr($ex->getMessage(), static::ERROR_SERVER_INTERRUPT)) {
-                    throw new RetryException('', 0, $ex);
+                foreach(static::ERROR_MYSQL_BREAK as $break_str){
+                    if (false !== strstr($ex->getMessage(), $break_str)) {
+                        throw new RetryException('', 0, $ex);
+                    }
                 }
                 throw $ex;
             }
